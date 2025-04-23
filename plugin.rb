9cf32690e3b827e0577ae90b37cf5fb3e04ffe12
Richard <richard@communiteq.com>
2025-04-23 11:49:50 +0000

    Fix: case insensitive comparison

--- plugin.rb
+++ plugin.rb
@@ -2,7 +2,7 @@
 
 # name: discourse-private-template-tagger
 # about: Tag private templates when users are unable to
-# version: 1.0
+# version: 1.0.1
 # authors: Communiteq
 # url: https://github.com/communiteq/discourse-private-template-tagger
 
@@ -14,8 +14,8 @@ after_initialize do
   on(:topic_created) do |topic|
     if SiteSetting.private_template_tagger_enabled && topic.private_message?
       user = topic.user
-      target_usernames = topic.allowed_users.pluck(:username) - [user.username]
-      if target_usernames.include?(SiteSetting.private_template_tagger_username)
+      target_usernames = topic.allowed_users.pluck(:username).map(&:downcase) - [user.username.downcase]
+      if target_usernames.include?(SiteSetting.private_template_tagger_username.downcase)
         guardian = Guardian.new(Discourse.system_user)
         tags = [ SiteSetting.private_template_tagger_tag ]
         DiscourseTagging.tag_topic_by_names(topic, guardian, tags, append: true)
